fb218f6132d3a1e2e34e9ebc5ce0b0ae805f8d74
Martin Teye <martinteye93@gmail.com>
2025-07-17 18:20:24 +0000

    Add relationship_app with ORM relationship models and queries

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,16 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# 1. Query all books by a specific author (e.g., author with id=1)
+books_by_author = Book.objects.filter(author_id=1)
+
+# 2. List all books in a library (e.g., library with id=1)
+library = Library.objects.get(id=1)
+books_in_library = library.books.all()
+
+# 3. Retrieve the librarian for a library (e.g., library with id=1)
+librarian = Librarian.objects.get(library_id=1)
+
+# Display results
+print("Books by Author:", books_by_author)
+print("Books in Library:", books_in_library)
+print("Librarian:", librarian.name)
