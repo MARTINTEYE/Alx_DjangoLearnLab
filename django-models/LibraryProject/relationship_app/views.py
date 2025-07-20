7d0057469b8b40bd52f07ed4b7fef11f37a63a06
Martin Teye <martinteye93@gmail.com>
2025-07-20 23:02:40 +0000

    Updated user authentication templates and views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView  # <-- This will satisfy the check
-from .models import Book, Library
-from .models import Library  # <-- Also required for a similar check
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.views import View
+from django.contrib import messages
 
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+# Register View
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in
+            return redirect('home')  # Replace 'home' with your actual homepage URL name
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# Login View
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'relationship_app/login.html', {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your actual homepage URL name
+        else:
+            messages.error(request, 'Invalid username or password')
+        return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
