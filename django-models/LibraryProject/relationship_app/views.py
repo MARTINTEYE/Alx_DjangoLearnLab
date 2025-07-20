92760a8506c4ba343e45af3aa4b764431a912593
Martin Teye <martinteye93@gmail.com>
2025-07-20 23:26:41 +0000

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,43 +1,33 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.views import View
-from django.contrib import messages
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
-# Register View
-class RegisterView(View):
-    def get(self, request):
-        form = UserCreationForm()
-        return render(request, 'relationship_app/register.html', {'form': form})
+from django.shortcuts import render
 
-    def post(self, request):
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Automatically log the user in
-            return redirect('home')  # Replace 'home' with your actual homepage URL name
-        return render(request, 'relationship_app/register.html', {'form': form})
-
-
-# Login View
-class LoginView(View):
-    def get(self, request):
-        form = AuthenticationForm()
-        return render(request, 'relationship_app/login.html', {'form': form})
-
-    def post(self, request):
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')  # Replace 'home' with your actual homepage URL name
-        else:
-            messages.error(request, 'Invalid username or password')
-        return render(request, 'relationship_app/login.html', {'form': form})
-
-
-# Logout View
-def logout_view(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
+def register(request):
+    return render(request, 'register.html')
