6e34b13d897547b2fc3c672ccdaed821b16b55ed
MARTIN TEYE <martinteye93@gmail.com>
2025-07-20 20:53:52 +0000

    Delete django-models directory

--- django-models/LibraryProject/relationship_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
