6b50c8feff7b3c99d851637020a7dbe6544ddf59
Martin Teye <martinteye93@gmail.com>
2025-07-21 21:02:14 +0000

    update on  UserProfile model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+# Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -13,35 +16,35 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+# Library model
 class Library(models.Model):
     name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
 
-    
-from django.db import models
-from django.contrib.auth.models import User
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
-
-    def __str__(self):
-        return self.user.username
-    from django.db import models
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
+# Librarian model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+# UserProfile model with role field
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
