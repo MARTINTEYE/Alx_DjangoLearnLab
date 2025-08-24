499ffdde0836a9cadfaa915e20a17a8cf10f2388
Martin Teye <martinteye93@gmail.com>
2025-08-24 19:13:55 +0000

    implementing feed functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,5 +6,28 @@ class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
+
+    # Self-referential ManyToMany for following relationships
+    following = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="followers",
+        blank=True
+    )
+
+    def follow(self, user):
+        """Follow another user."""
+        self.following.add(user)
+
+    def unfollow(self, user):
+        """Unfollow a user."""
+        self.following.remove(user)
+
+    def is_following(self, user):
+        return self.following.filter(id=user.id).exists()
+
     def __str__(self):
         return self.username
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,11 +1,17 @@
-from django.contrib.auth import authenticate
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
+from django.contrib.auth import authenticate # type: ignore
+from rest_framework.response import Response # pyright: ignore[reportMissingImports]
+from rest_framework.views import APIView # pyright: ignore[reportMissingImports]
+from rest_framework.authtoken.models import Token # pyright: ignore[reportMissingImports]
+from rest_framework.permissions import IsAuthenticated # pyright: ignore[reportMissingImports]
+from rest_framework import status # pyright: ignore[reportMissingImports]
 from .serializers import RegisterSerializer, UserSerializer
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model # pyright: ignore[reportMissingModuleSource]
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -34,3 +40,17 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    target_user = get_object_or_404(CustomUser, id=user_id)
+    request.user.follow(target_user)
+    return Response({"message": f"You are now following {target_user.username}"}, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    target_user = get_object_or_404(CustomUser, id=user_id)
+    request.user.unfollow(target_user)
+    return Response({"message": f"You have unfollowed {target_user.username}"}, status=status.HTTP_200_OK)
