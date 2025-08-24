d404a38076c6f7a4f7ca342c6b9f6f76203eb954
Martin Teye <martinteye93@gmail.com>
2025-08-24 19:44:39 +0000

    Develop Notification System

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,7 +1,9 @@
+# posts/models.py
 from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
@@ -23,3 +25,15 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("post", "user")  # Prevent multiple likes by same user
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
